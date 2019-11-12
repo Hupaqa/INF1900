0a25789c83c7d28e1deedafca8448769db402bd2
Jordan Lecourtois <jolecf@l3818-10.info.polymtl.ca>
2019-11-12 13:57:07 -0500

    Modif suiveurLigne

--- Robot/robot.cpp
+++ Robot/robot.cpp
@@ -10,67 +10,134 @@ U5 = PC7
 
 #define F_CPU 8000000UL
 
-#include "suiveurLigne/suiveurLigne.h"
+#include "suiveurLigne.h"
+#include "navigator.h"
 #include "delay.h"
 
+uint8_t VITESSE = 96;
+
 enum ETAT {
+    INIT,
     COULOIR,
     MUR, 
     COUPURE,
     BOUCLE
 };
 
-enum ETAT_COUPURE{
-    TOUR_DROIT_1,
-    TOUR_GAUCHE_1,
-    TOUR_DROIT_2,
-    TOUR_GAUCHE_2
+enum ETAT_BOUCLE{
+    INIT
 };
 
-volatile ETAT etatPresent = COULOIR;
+enum ETAT_COUPURE{
+    DEBUT,
+    COUPURE1,
+    COUPURE2,
+    COUPURE3,
+    COUPURE4,
+    FIN
+}
 
-/*ETAT etatInitial() {
-    return INIT;
-}*/
+volatile ETAT etatPresent = ETAT::INIT;
+volatile ETAT_BOUCLE etatBoucle = INJT;
+volatile ETAT_COUPURE etatCoupure = ETAT_COUPURE::DEBUT;
 
-void suivreLigne(){
+
+bool suivreLigne(){
     if (!(PINC & 0b00010000)){
         if(PINC & 0b01000000){
-            suiveurLigne::redressementDroit();
+            suiveurLigne::redressementDroit(VITESSE);
         }
         else if (PINC & 0b00000100) {
-            suiveurLigne::redressementGauche();
+            suiveurLigne::redressementGauche(VITESSE);
         }
-    }else if ((PINC & 0b00010000)&&(PINC & 0b00001000)&&(PINC & 0b00000100)){
-        suiveurLigne::tournerGauche();
+        return true;
+    }else if (!(PINC & 0b01111100)){
+        ajustementPWM(0, 0, 0, 0)
+        return false;
     }
     else{
-        ajustementPWM(128, 0, 128, 0);
+        ajustementPWM(VITESSE, 0, VITESSE, 0);
+        return true;
     }
 }
-/*
-void changer_etat(){
+
+void changeSate(){
     switch(etatPresent){
-        case COULOIR : etatPresent = MUR; break;
-        case MUR : etatPresent = BOUCLE ; break;
-        case BOUCLE : etatPresent = COUPURE ; break;
-        case COUPURE : etatPresent = COULOIR ; break;
-        case default : break;
+        case ETAT::INIT :
+            etatPresent = COUPURE;
+            break;
+            
+        case ETAT::BOUCLE :
+            
+            break;
+        case ETAT::COUPURE : 
+            if(etatCoupure == ETAT_COUPURE::FIN) 
+                etatPresent = ETAT::COULOIR;
+            break;
+        case ETAT::COULOIR : 
+
+            break;
+        case ETAT::MUR :
+        
+            break;    
     }
 }
-*/
 
+void doAction(){
+    switch(etatPresent){
+        case ETAT::INIT :
+            delay_ms(100);
+            break;
+        case ETAT::COULOIR :
 
+            break;
+        case ETAT::COUPURE :
+            actionCoupure();
+            break;
+        case ETAT::BOUCLE :
 
+            break;
+        case ETAT::MUR :
+
+            break;    
+    }
+}
+
+void actionCoupure(){
+    switch(etatCoupure){
+        case ETAT_COUPURE::DEBUT :
+
+            break;
+        case ETAT_COUPURE::    
+    }
+}
+
+void actionCouloir(){
+
+    while (PINC & 0b01111100)
+
+    ajustementPWM(VITESSE, 0, VITESSE, 0);
+    
+    while (!(PINC & 0b00111000))
+    {
+        if (PINC & 0b01000000) {
+            suiveurLigne::redressementDroit(VITESSE);
+        }   
+        else if (PINC & 0b00000100){
+            suiveurLigne::redressementGauche(VITESSE);
+        }
+    }
+}
 
 int main(){
     DDRC = 0x00;
     DDRD = 0xff;
 
-    while (!(PIND&0x04));
 
+    ajustementPWM(VITESSE, 0, VITESSE, 0);
     while(true){
         suivreLigne();
+        delay_ms(5);
     }
     return 0;
 }
--- lib_dir/suiveurLigne.cpp
+++ lib_dir/suiveurLigne.cpp
@@ -4,26 +4,26 @@
 
 using namespace suiveurLigne;
 
-void suiveurLigne::redressementDroit(){
-    ajustementPWM(32, 0, 96, 0);
+void suiveurLigne::redressementDroit(uint8_t vitesse){
+    ajustementPWM(vitesse/3, 0, vitesse, 0);
     delay_ms (50);
-    ajustementPWM(96, 0, 96, 0);
+    ajustementPWM(vitesse, 0, vitesse, 0);
 }
 
-void suiveurLigne::redressementGauche(){
-    ajustementPWM(96, 0, 32, 0);
+void suiveurLigne::redressementGauche(uint8_t vitesse){
+    ajustementPWM(vitesse, 0, vitesse/3, 0);
     delay_ms (50);
-    ajustementPWM(96, 0, 96, 0);
+    ajustementPWM(vitesse, 0, vitesse, 0);
 }
 
-void suiveurLigne::tournerDroit(){
-    ajustementPWM(0, 0, 128, 0);
-    delay_ms (500);
-    ajustementPWM(128, 0, 128, 0);
+void suiveurLigne::tournerDroit(uint8_t vitesse){
+    ajustementPWM(0, 0, vitesse, 0);
+    while(!(PINC & 0b01110000));
+    ajustementPWM(vitesse, 0, vitesse, 0);
 }
 
-void suiveurLigne::tournerGauche(){
-    ajustementPWM(128, 0, 0, 0);
-    delay_ms (500);
-    ajustementPWM(128, 0, 128, 0);
+void suiveurLigne::tournerGauche(uint8_t vitesse){
+    ajustementPWM(vitesse, 0, 0, 0);
+    while(!(PINC & 0b00011100));
+    ajustementPWM(vitesse, 0, vitesse, 0);
 }
