dff163bcff6f46d26e30f1e170e86d05619c427c
Hugo Paquin <paqh@hotmail.com>
2019-11-29 18:48:25 -0500

    ajout table etat selection.h

--- src/selection.h
+++ src/selection.h
@@ -22,7 +22,19 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-
+/*
+╔═══════════════════════════════════════════════════╗
+║                  Table des états                  ║
+╠══════════════╦══════════════════════╦═════════════╣
+║ État courant ║ interruptDebounced() ║  État futur ║
+╠══════════════╬══════════════════════╬═════════════╣
+║   selection  ║         true         ║   appeler   ║
+╠══════════════╬══════════════════════╬═════════════╣
+║   selection  ║         false        ║  selection  ║
+╠══════════════╬══════════════════════╬═════════════╣
+║    appeler   ║           X          ║ afficherFin ║
+╚══════════════╩══════════════════════╩═════════════╝
+*/
 
 #ifndef SELECTION_H
 #define SELECTION_H
